4909a89fbda820250483f62f6cbdf695ca4af9bd
panacea-vetcare <vetcare.panacea@gmail.com>
2024-08-04 01:41:13 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Animal pregnency kits",
-  description: "Animal pregnency kits",
+  title: "Bovine pregnancy kit",
+  description: "Bovine pregnancy kit",
 };
 
 export default function RootLayout({
