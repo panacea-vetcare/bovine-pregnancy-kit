38e1fb4c74f40ee1a3e3f8f8a081028bceb22026
panacea-vetcare <vetcare.panacea@gmail.com>
2024-08-07 16:14:22 +0530

    Update bovinepdkit.tsx

--- src/components/bovinepdkit.tsx
+++ src/components/bovinepdkit.tsx
@@ -1,23 +1,18 @@
-import Image from "next/image";
-import readyToUse from "public/ready-to-use.jpg";
-import kit from "public/kit.jpg";
-import checkmark from "public/checkmark.jpg";
-import Card from "@/components/card";
 
 export default function Bovinepdkit() {
   return (
     <>
       <p id="bovinepdkit"></p>
       <div className="container-spacing">
-        <p className="text-center text-2xl">About us</p>
+        <p className="text-center text-2xl">Bovine Pregnancy Diagnosis Rapid Test Kit (PD Kit)</p>
         <div className="mt-4">
-          The Bovine Pregnancy Diagnostic Rapid Test Kit is a reliable, efficient,
+          The Bovine Pregnancy Diagnosis Rapid Test Kit is a reliable, efficient,
           and user-friendly tool designed for the rapid detection of pregnancy in cattle.
           This kit employs advanced immunoassay technology to provide accurate results
           within a short period, making it an invaluable asset for veterinarians, farmers, and livestock managers.
         </div>
         <div className="mt-4">
-          <div>Key Features</div>
+          <div className="mt-6">Key Features</div>
           <div>Rapid Results: Provides results within minutes, allowing for quick decision-making and efficient herd management.</div>
         </div>
 
