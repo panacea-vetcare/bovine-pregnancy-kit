0b3578d48163f2564286d9331f93845b58e2c926
panacea-vetcare <vetcare.panacea@gmail.com>
2024-08-08 10:06:26 +0530

    Update bovinepdkit.tsx

--- src/components/bovinepdkit.tsx
+++ src/components/bovinepdkit.tsx
@@ -15,29 +15,32 @@ export default function Bovinepdkit() {
         <div className="mt-4">
           <p className="text-xl">Key Features</p>
           <div><b>Rapid Results:</b> Provides results within minutes, allowing for quick decision-making and efficient herd management.</div>
-          <div>High Accuracy: Boasts a high sensitivity and specificity, ensuring reliable detection of pregnancy from as early as 28 days post-insemination.</div>
-          <div>User-Friendly: Simple to use with minimal training required. The kit includes all necessary components and easy-to-follow instructions.</div>
-          <div>Non-Invasive: Utilizes a small blood sample, minimizing stress and discomfort for the animals.</div>
-          <div>Cost-Effective: Reduces the need for expensive and time-consuming diagnostic methods like ultrasound, making it a cost-effective solution for regular herd monitoring.</div>
+          <div><b>High Accuracy:</b> Boasts a high sensitivity and specificity, ensuring reliable detection of pregnancy from as early as 28 days post-insemination.</div>
+          <div><b>User-Friendly:</b> Simple to use with minimal training required. The kit includes all necessary components and easy-to-follow instructions.</div>
+          <div><b>Non-Invasive:</b> Utilizes a small blood sample, minimizing stress and discomfort for the animals.</div>
+          <div><b>Cost-Effective:</b> Reduces the need for expensive and time-consuming diagnostic methods like ultrasound, making it a cost-effective solution for regular herd monitoring.</div>
         </div>
         <div className="mt-4">
           <p className="text-xl">How It Works</p>
-          <div>Sample Collection: Collect a small blood sample from the cow using the provided collection tools.</div>
-          <div>Test Procedure: Apply the sample to the test device as per the instructions provided in the kit. Wait for the specified time (typically a few minutes) for the test to develop.</div>
-          <div>Result Interpretation: Indicates the presence or absence of pregnancy-associated glycoproteins (PAGs) in the sample, confirming the pregnancy.</div>
-          <div>Quality Control: Each kit includes a control line to ensure the test has been performed correctly.</div>
+          <div><b>Sample Collection:</b> Collect a small blood sample from the cow using the provided collection tools.</div>
+          <div><b>Test Procedure:</b> Apply the sample to the test device as per the instructions provided in the kit. Wait for the specified time (typically a few minutes) for the test to develop.</div>
+          <div><b>Result Interpretation:</b> Indicates the presence or absence of pregnancy-associated glycoproteins (PAGs) in the sample, confirming the pregnancy.</div>
+          <div><b>Quality Control:</b> Each kit includes a control line to ensure the test has been performed correctly.</div>
         </div>
         <div className="mt-4">
           <p className="text-xl">Benefits</p>
-          <div>Improved Reproductive Management: Helps in early identification of pregnant and non-pregnant cows, allowing for timely intervention and better reproductive planning.</div>
-          <div>Enhanced Productivity: Early pregnancy detection aids in optimizing breeding schedules and improving overall herd productivity.</div>
-          <div>Animal Welfare: Non-invasive and quick testing minimizes animal handling and stress.</div>
-          <div>Quality Control: Each kit includes a control line to ensure the test has been performed correctly.</div>
+          <div><b>Improved Reproductive Management:</b> Helps in early identification of pregnant and non-pregnant cows, allowing for timely intervention and better reproductive planning.</div>
+          <div><b>Enhanced Productivity:</b> Early pregnancy detection aids in optimizing breeding schedules and improving overall herd productivity.</div>
+          <div><b>Animal Welfare:</b> Non-invasive and quick testing minimizes animal handling and stress.</div>
+          <div><b>Quality Control:</b> Each kit includes a control line to ensure the test has been performed correctly.</div>
         </div>
         <div className="mt-4">
           <p className="text-xl">Customer Support</p>
-          <div>For technical assistance or to reorder kits, please contact our customer service.
-            Our team of experts is available to support you with any queries or concerns you may have.</div>
+          <div>For technical assistance or to reorder kits, please contact our 
+            <Link href="#contactUs" className="links">
+              customer service
+            </Link>
+            .Our team of experts is available to support you with any queries or concerns you may have.</div>
         </div>
       </div>
     </>
