9740924a2a8d723d7fbdd65b3b61b2919b134dea
panacea-vetcare <vetcare.panacea@gmail.com>
2024-08-08 10:29:08 +0530

    Update bovinepdkit.tsx

--- src/components/bovinepdkit.tsx
+++ src/components/bovinepdkit.tsx
@@ -36,11 +36,7 @@ export default function Bovinepdkit() {
         </div>
         <div className="mt-4">
           <p className="text-xl">Customer Support</p>
-          <div>For technical assistance or to reorder kits, please contact our 
-            <Link href="#contactUs" className="links text-">
-              customer service
-            </Link>
-            .Our team of experts is available to support you with any queries or concerns you may have.</div>
+          <div>For technical assistance or to reorder kits, please contact our customer service. Our team of experts is available to support you with any queries or concerns you may have.</div>
         </div>
       </div>
     </>
